d36ef0cf02d74f0e54702bc0ed65ffde702bca7b
EtudiantDevWeb <dalilah.amouri@free.fr>
2022-05-20 13:37:55 +0200

    Ajout produit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -23,11 +23,39 @@ class AdminController extends AbstractController
 
         $produit= new Produit();
         // ici on instancie un nouvel objet Produit vide, que l'on va charger avec les informations réceptionnées du formulaire grace à Request
-
+        dump($produit);
         $form=$this->createForm(ProduitType::class, $produit);
         // ici on instancie un objet Form qui va controller automatiquement la correspondance des champs de formulaire demandés dans ProduitType avec les propriétés de notre entité Produit
         // La méthode createForm() attend 2 arguments, le 1er le nom du formulaire (le type) à utiliser , en second l'entité correspondante à ce formulaire
 
+        $form->handleRequest($request);//  on utilise la méthode handlerequest() de notre objet $form afin de traiter les données
+
+        dump($produit);// ici l'objet produit est rempli des données du formulaire
+
+        if ($form->isSubmitted() && $form->isValid()){// si le formulaire a été soumis via notre boutton submit et que les controles de contraintes effectué sur notre entité et notre formulaire sont correct
+
+            $image=$form->get('photo')->getData();
+            //dd($image);
+
+            $nomImage=date('YmdHis').'_'.$image->getClientOriginalName();
+            // on renomme le fichier photo pour s'assurer de son unicité
+
+            $image->move($this->getParameter('images_directory'), $nomImage);
+            // on utilise la méthode move() équivalente à la fonction copy() en php.
+            // afin d'uploader le fichier temporaire dans l'app
+
+            $produit->setPhoto('upload/'.$nomImage);
+
+            $manager->persist($produit);// ici on prépare la requête
+            $manager->flush(); // on execute
+
+            return $this->redirectToRoute('home');
+
+
+
+
+
+        }
 
 
 
